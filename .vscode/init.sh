048d48bca96938ffc8b6c2c5a2c8d48e0e22a62f
Abdulrahman Annaba <abdulrahman.annaba@gmail.com>
2024-09-15 13:29:04 -0500

    Clarified commenst in bash script

--- .vscode/init.sh
+++ .vscode/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source ~/.bashrc
-# Tells CMake the location of the compiler binary
+# Tells CMake the location of the compiler binary. Set this to your compiler location
 export CMAKE_C_COMPILER="/usr/bin/avr-gcc"
-# Tells CMake which build system to use to actually build the project
+# Tells CMake which build system to use to actually build the project. Set this to your build generator
 export CMAKE_GENERATOR="Ninja"
-# Toolchain-specific binaries for building/flashing
+# Toolchain-specific binaries for building/flashing. Set these to their respective locations
 export OBJCOPY_PATH="/usr/bin/avr-objcopy"
 export AVRSTRIP_PATH="/usr/bin/avr-strip"
 export AVRDUDE_PATH="/usr/bin/avrdude"
